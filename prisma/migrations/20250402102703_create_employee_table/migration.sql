77538376c22594a153ace3aa8c800afe293a66b7
Joe Kim <joesm.genie@gmail.com>
2025-04-02 17:27:31 +0700

    create migration for table employee

---
+++ prisma/migrations/20250402102703_create_employee_table/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "Employee" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone" TEXT NOT NULL,
+    "mobile" VARCHAR(10),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Employee_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_username_key" ON "Employee"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_email_key" ON "Employee"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Employee_phone_key" ON "Employee"("phone");
