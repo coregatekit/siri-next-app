c9ec39f923a9f51e043d31c6558004017b8f1a5c
Joe Kim <joesm.genie@gmail.com>
2025-04-04 01:05:39 +0700

    remove validate confirm password

--- app/api/employees/create/route.ts
+++ app/api/employees/create/route.ts
@@ -13,9 +13,6 @@ const CreateUserFormSchema = z.object({
 	password: z
 		.string()
 		.min(8, { message: 'Password must be at least 8 characters long' }),
-	confirmPassword: z
-		.string()
-		.min(8, { message: 'Password must be at least 8 characters long' }),
 	name: z
 		.string()
 		.min(2, { message: 'Name must be at least 2 characters long' }),
