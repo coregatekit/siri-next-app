201ed1368805bf1b64335bd364e1b90fe28ddc76
Joe Kim <joesm.genie@gmail.com>
2025-04-03 23:59:00 +0700

    add unit test for method hashPassword

---
+++ services/encryptions/index.test.ts
@@ -0,0 +1,37 @@
+import * as argon2 from 'argon2';
+import { EncryptionService } from '.';
+
+jest.mock('argon2', () => ({
+	hash: jest.fn(),
+	argon2id: 2,
+}));
+
+describe('EncryptionService', () => {
+	let service: EncryptionService;
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+		service = new EncryptionService();
+	});
+
+	describe('hashPassword', () => {
+		it('should hash a password', async () => {
+      // Arrange
+      const plaintext = 'securePassword999';
+      const expectedHash = 'hashedPassword123';
+      (argon2.hash as jest.Mock).mockReturnValue(expectedHash);
+
+      // Act
+      const result = await service.hashPassword(plaintext);
+
+      // Assert
+      expect(result).toBe(expectedHash);
+      expect(argon2.hash).toHaveBeenCalledWith(plaintext, {
+        type: argon2.argon2id,
+        memoryCost: 65536,
+        timeCost: 4,
+        parallelism: 4,
+      });
+    });
+	});
+});
