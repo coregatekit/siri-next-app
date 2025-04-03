c109937cb7f1b9dc83fb9a24414b98548ff9fcac
Joe Kim <joesm.genie@gmail.com>
2025-04-04 00:52:09 +0700

    add validate body

--- app/api/employees/create/route.ts
+++ app/api/employees/create/route.ts
@@ -4,16 +4,37 @@ import { type NextRequest, NextResponse } from 'next/server';
 import { EmployeeService } from '@/services/employees';
 import { EncryptionService } from '@/services/encryptions';
 import type { CreateEmployeeData } from '@/services/employees/interfaces';
+import { z } from 'zod';
+
+const CreateUserFormSchema = z.object({
+	username: z
+		.string()
+		.min(6, { message: 'Username must be at least 6 characters long' }),
+	password: z
+		.string()
+		.min(8, { message: 'Password must be at least 8 characters long' }),
+	confirmPassword: z
+		.string()
+		.min(8, { message: 'Password must be at least 8 characters long' }),
+	name: z
+		.string()
+		.min(2, { message: 'Name must be at least 2 characters long' }),
+	email: z.string().email({ message: 'Invalid email address' }),
+	mobile: z.string().optional(),
+});
 
 export async function POST(request: NextRequest, response: NextResponse) {
 	try {
 		const body = await request.json();
+
+		const validatedData = CreateUserFormSchema.parse(body);
+
 		const createEmployeeData: CreateEmployeeData = {
-			username: body.username,
-			password: body.password,
-			name: body.name,
-			email: body.email,
-			mobile: body.mobile,
+			username: validatedData.username,
+			password: validatedData.password,
+			name: validatedData.name,
+			email: validatedData.email,
+			mobile: validatedData.mobile,
 		};
 
 		const employeeService = new EmployeeService(
@@ -25,16 +46,23 @@ export async function POST(request: NextRequest, response: NextResponse) {
 
 		return NextResponse.json(newEmployee, { status: 201 });
 	} catch (error) {
+		if (error instanceof z.ZodError) {
+			return NextResponse.json(
+				{ message: 'Validation error', errors: error.errors },
+				{ status: 400 },
+			);
+		}
+
 		if (error instanceof Error) {
 			if (
 				error.message === 'Username already exists' ||
 				error.message === 'Email already exists'
 			) {
-				return NextResponse.json({ error: error.message }, { status: 409 });
+				return NextResponse.json({ message: error.message }, { status: 409 });
 			}
 		}
 		return NextResponse.json(
-			{ error: 'Internal Server Error' },
+			{ message: 'Internal Server Error' },
 			{ status: 500 },
 		);
 	}
