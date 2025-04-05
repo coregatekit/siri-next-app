bffb5fd2f5f27bbe2e27ee1a1fd775a0ea5475b9
Joe Kim <joesm.genie@gmail.com>
2025-04-05 12:48:11 +0700

    fix route fetch employee list

--- app/admin/components/user-list.tsx
+++ app/admin/components/user-list.tsx
@@ -3,28 +3,14 @@ import type { Employee } from '@/app/types/employees';
 import { DataTable } from '@/components/ui/data-table';
 import { EmployeeColumn } from './user-list-columns';
 
-const mockEmployeeData: Employee[] = [
-	{
-		id: 'test3',
-		username: 'test3',
-		name: 'Test User 3',
-		email: 'tester3@example.com',
-		mobile: '1234567892',
-		isAdmin: false,
-		isSetPw: false,
-		createdAt: new Date('2025-01-01'),
-		updatedAt: new Date('2025-01-02'),
-	},
-];
-
 function UserList() {
-	const [employees, setEmployees] = useState<Employee[]>(mockEmployeeData);
+	const [employees, setEmployees] = useState<Employee[]>([]);
 	const [isLoading, setIsLoading] = useState<boolean>(false);
 
 	useEffect(() => {
 		const fetchEmployees = async () => {
 			setIsLoading(true);
-			const response = await fetch('/api/admin/employees');
+			const response = await fetch('/api/employees');
 			if (response.status === 200) {
 				const data = await response.json();
 				setEmployees(data);
