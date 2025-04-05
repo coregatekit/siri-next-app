e19cf9e8605b83963637972c9811945ce290e385
Joe Kim <joesm.genie@gmail.com>
2025-04-05 11:07:02 +0700

    implement fetch employee list

--- app/admin/components/user-list.tsx
+++ app/admin/components/user-list.tsx
@@ -1,63 +1,9 @@
+import { useEffect, useState } from 'react';
 import type { Employee } from '@/app/types/employees';
 import { DataTable } from '@/components/ui/data-table';
 import { EmployeeColumn } from './user-list-columns';
 
 const mockEmployeeData: Employee[] = [
-	{
-		id: 'test1',
-		username: 'test1',
-		name: 'Test User 1',
-		email: 'tester1@example.com',
-		mobile: '1234567890',
-		isAdmin: true,
-		isSetPw: true,
-		createdAt: new Date('2025-01-01'),
-		updatedAt: new Date('2025-01-02'),
-	},
-	{
-		id: 'test2',
-		username: 'test2',
-		name: 'Test User 2',
-		email: 'tester2@example.com',
-		mobile: '1234567891',
-		isAdmin: false,
-		isSetPw: true,
-		createdAt: new Date('2025-01-01'),
-		updatedAt: new Date('2025-01-02'),
-	},
-	{
-		id: 'test3',
-		username: 'test3',
-		name: 'Test User 3',
-		email: 'tester3@example.com',
-		mobile: '1234567892',
-		isAdmin: false,
-		isSetPw: false,
-		createdAt: new Date('2025-01-01'),
-		updatedAt: new Date('2025-01-02'),
-	},
-	{
-		id: 'test1',
-		username: 'test1',
-		name: 'Test User 1',
-		email: 'tester1@example.com',
-		mobile: '1234567890',
-		isAdmin: true,
-		isSetPw: true,
-		createdAt: new Date('2025-01-01'),
-		updatedAt: new Date('2025-01-02'),
-	},
-	{
-		id: 'test2',
-		username: 'test2',
-		name: 'Test User 2',
-		email: 'tester2@example.com',
-		mobile: '1234567891',
-		isAdmin: false,
-		isSetPw: true,
-		createdAt: new Date('2025-01-01'),
-		updatedAt: new Date('2025-01-02'),
-	},
 	{
 		id: 'test3',
 		username: 'test3',
@@ -72,9 +18,30 @@ const mockEmployeeData: Employee[] = [
 ];
 
 function UserList() {
+	const [employees, setEmployees] = useState<Employee[]>(mockEmployeeData);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		const fetchEmployees = async () => {
+			setIsLoading(true);
+			const response = await fetch('/api/admin/employees');
+			if (response.status === 200) {
+				const data = await response.json();
+				setEmployees(data);
+			}
+			setIsLoading(false);
+		};
+
+		fetchEmployees();
+	}, []);
+
+	if (isLoading) {
+		return <div>Loading...</div>;
+	}
+
 	return (
 		<div className='my-4 bg-white rounded-lg'>
-			<DataTable columns={EmployeeColumn} data={mockEmployeeData} />
+			<DataTable columns={EmployeeColumn} data={employees} />
 		</div>
 	);
 }
