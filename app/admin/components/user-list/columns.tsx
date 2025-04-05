e135daa38fa4916accc5b47a9ff0e6a27a83a655
Joe Kim <joesm.genie@gmail.com>
2025-04-05 09:56:15 +0700

    implement data table column header

---
+++ app/admin/components/user-list/columns.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+import type { Employee } from '@/app/types/employees';
+import type { ColumnDef } from '@tanstack/react-table';
+
+export const columns: ColumnDef<Employee>[] = [
+	{
+		accessorKey: 'id',
+		header: 'ID',
+	},
+	{
+		accessorKey: 'username',
+		header: 'Username',
+	},
+	{
+		accessorKey: 'name',
+		header: 'Name',
+	},
+	{
+		accessorKey: 'email',
+		header: 'Email',
+	},
+	{
+		accessorKey: 'mobile',
+		header: 'Mobile',
+	},
+	{
+		accessorKey: 'isAdmin',
+		header: 'Admin',
+	},
+	{
+		accessorKey: 'isSetPw',
+		header: 'Already Set Password',
+	},
+	{
+		accessorKey: 'updatedAt',
+		header: 'Latest Update',
+	},
+];
