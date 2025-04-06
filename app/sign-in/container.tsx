a45ed407b18dbc2d7229ba53d9e775e284c2b238
Joe Kim <joesm.genie@gmail.com>
2025-04-06 14:33:19 +0700

    fix render with client

--- app/sign-in/container.tsx
+++ app/sign-in/container.tsx
@@ -4,17 +4,12 @@ import React from 'react';
 import SignInBox from '@/components/sign-in';
 import type { SignInFormData } from '../types/sign-in';
 
-
 function SignInContainer() {
 	const handleSignIn = (data: SignInFormData) => {
 		// Handle login logic here
 	};
 
-	return (
-		<div>
-			<SignInBox handleSignIn={handleSignIn} />
-		</div>
-	);
+	return <SignInBox handleSignIn={handleSignIn} />;
 }
 
 export default SignInContainer;
