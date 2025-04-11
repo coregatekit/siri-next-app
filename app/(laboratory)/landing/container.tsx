c6a5f7a9cda16dc4413c36c335151b8fa17f80bc
Joe Kim <joesm.genie@gmail.com>
2025-04-11 09:41:54 +0700

    add section laboratory

---
+++ app/(laboratory)/landing/container.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LandingContainer() {
+	return <div>LandingContainer</div>;
+}
+
+export default LandingContainer;
---
+++ app/(laboratory)/landing/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LandingContainer from './container';
+
+function Landing() {
+	return <LandingContainer />;
+}
+
+export default Landing;
