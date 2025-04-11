9d04669e9356d51900551528e09cf0fd2ee95415
Joe Kim <joesm.genie@gmail.com>
2025-04-11 20:57:07 +0700

    implement method get todos

--- __tests__/services/todos/index.test.ts
+++ __tests__/services/todos/index.test.ts
@@ -1,6 +1,6 @@
 import { TodoService } from '@/services/todos';
 import type { ITodoService } from '@/services/todos/interfaces';
-import type { PrismaClient } from '@prisma/client';
+import type { PrismaClient, Todo } from '@prisma/client';
 import { mockDeep, type DeepMockProxy } from 'jest-mock-extended';
 
 jest.mock('@prisma/client', () => ({
@@ -9,23 +9,54 @@ jest.mock('@prisma/client', () => ({
 
 describe('Todos Service', () => {
 	let service: ITodoService;
-  let prisma: DeepMockProxy<PrismaClient>;
+	let prisma: DeepMockProxy<PrismaClient>;
 
 	beforeEach(() => {
-    jest.clearAllMocks();
-    prisma = mockDeep<PrismaClient>();
+		jest.clearAllMocks();
+		prisma = mockDeep<PrismaClient>();
 		service = new TodoService(prisma);
 	});
 
 	describe('getTodos', () => {
-    it('should return an empty array', async () => {
+		const todo1: Todo = {
+			id: '1',
+			title: 'Todo 1',
+      description: 'Description 1',
+			done: false,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		};
+		const todo2: Todo = {
+			id: '2',
+			title: 'Todo 2',
+      description: 'Description 1',
+			done: false,
+			createdAt: new Date(),
+			updatedAt: new Date(),
+		};
+
+		it('should return an empty array', async () => {
+			// Arrange
+      prisma.todo.findMany.mockResolvedValue([]);
+
+			// Act
+			const result = await service.getTodos();
+
+			// Assert
+			expect(result).toEqual([]);
+      expect(prisma.todo.findMany).toHaveBeenCalled();
+		});
+
+    it('should return a list of todos', async () => {
       // Arrange
+      prisma.todo.findMany.mockResolvedValue([todo1, todo2]);
 
       // Act
       const result = await service.getTodos();
 
       // Assert
-      expect(result).toEqual([]);
+      expect(result).toEqual([todo1, todo2]);
+      expect(prisma.todo.findMany).toHaveBeenCalled();
     });
-  });
+	});
 });
--- services/todos/index.ts
+++ services/todos/index.ts
@@ -2,13 +2,14 @@ import type { PrismaClient, Todo } from '@prisma/client';
 import type { ITodoService } from './interfaces';
 
 export class TodoService implements ITodoService {
-  private readonly prisma: PrismaClient;
+	private readonly prisma: PrismaClient;
 
-  constructor(prisma: PrismaClient) {
-    this.prisma = prisma;
-  }
+	constructor(prisma: PrismaClient) {
+		this.prisma = prisma;
+	}
 
-  async getTodos(): Promise<Todo[]> {
-    return [];
-  }
+	async getTodos(): Promise<Todo[]> {
+		const todos = await this.prisma.todo.findMany();
+		return todos;
+	}
 }
