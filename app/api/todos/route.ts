292357c182fcd3ca20389df12618672e2fa3bf6e
Joe Kim <joesm.genie@gmail.com>
2025-04-11 21:13:23 +0700

    add api route for create todo

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -3,17 +3,36 @@ import { NextResponse, type NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest, response: NextResponse) {
 	try {
-    const todoService = getTodoService();
-    const todos = await todoService.getTodos();
+		const todoService = getTodoService();
+		const todos = await todoService.getTodos();
 
-    return NextResponse.json(todos, { status: 200 });
+		return NextResponse.json(todos, { status: 200 });
 	} catch (error) {
-    if (error instanceof Error) {
-      return NextResponse.json({ message: error.message }, { status: 500 });
-    }
-    return NextResponse.json(
-      { message: 'Internal server error' },
-      { status: 500 },
-    );
-  }
+		if (error instanceof Error) {
+			return NextResponse.json({ message: error.message }, { status: 500 });
+		}
+		return NextResponse.json(
+			{ message: 'Internal server error' },
+			{ status: 500 },
+		);
+	}
+}
+
+export async function POST(request: NextRequest, response: NextResponse) {
+	try {
+    const body = await request.json();
+
+    const todoService = getTodoService();
+    const todo = await todoService.createTodo(body);
+
+    return NextResponse.json(todo, { status: 201 });
+	} catch (error) {
+		if (error instanceof Error) {
+			return NextResponse.json({ message: error.message }, { status: 500 });
+		}
+		return NextResponse.json(
+			{ message: 'Internal server error' },
+			{ status: 500 },
+		);
+	}
 }
