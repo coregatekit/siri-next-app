b2d4ac80138499be0a59b039251769d5ced9d975
Joe Kim <joesm.genie@gmail.com>
2025-04-11 21:41:18 +0700

    render the todos list

--- app/(laboratory)/todo/container.tsx
+++ app/(laboratory)/todo/container.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+'use client';
+
+import type { Todo } from '@prisma/client';
+import React, { useEffect, useState } from 'react';
 
 function TodoContainer() {
-	return <div>TodoContainer</div>;
+	const [todos, setTodos] = useState<Todo[]>([]);
+	const [loading, setLoading] = useState<boolean>(false);
+
+	useEffect(() => {
+		const fetchTodos = async () => {
+			setLoading(true);
+
+			const response = await fetch('/api/todos');
+			if (!response.ok) {
+				throw new Error('Failed to fetch todos');
+			}
+
+			const data = await response.json();
+			setTodos(data);
+			setLoading(false);
+		};
+
+		fetchTodos().catch((error) => {
+			console.error('Error fetching todos:', error);
+			setLoading(false);
+		});
+	}, []);
+
+	if (loading) {
+		return (
+			<div className='flex justify-center items-center mt-8'>
+				<h1>Loading...</h1>
+			</div>
+		);
+	}
+
+	return (
+		<div className='flex flex-col items-stretch justify-center mt-8'>
+			<h1 className='text-3xl font-bold text-slate-700 px-12'>Todo list</h1>
+			<div>
+				{todos.length === 0 ? (
+					<p>No todos available</p>
+				) : (
+					todos.map((todo) => (
+						<div
+							key={todo.id}
+							className='flex justify-between items-center px-12 p-4 border-b border-gray-300'
+						>
+							<div className='flex flex-row gap-4 justify-center items-center'>
+								<span className='text-lg font-bold text-slate-700'>
+									{todo.title}
+								</span>
+								<span className='text-slate-500'>{todo.description}</span>
+							</div>
+							<span>{todo.done ? '✅' : '❌'}</span>
+						</div>
+					))
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default TodoContainer;
