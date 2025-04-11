9c6a3fcff9a94eb466d0dc6639378f95bfba6327
Joe Kim <joesm.genie@gmail.com>
2025-04-11 10:38:39 +0700

    fix layout for page login to ignore navbar

--- components/nav-bar-wrapper.tsx
+++ components/nav-bar-wrapper.tsx
@@ -4,23 +4,23 @@ import { usePathname } from 'next/navigation';
 import React from 'react';
 
 function NavBarWrapper() {
-	const pathName = usePathname();
-	const excludedPaths = ['/sign-in', '/login'];
+  const pathName = usePathname();
+  const excludedPaths = ['/sign-in', '/login'];
 
-	if (excludedPaths.includes(pathName)) {
-		return null;
-	}
+  if (excludedPaths.includes(pathName)) {
+    return null;
+  }
 
-	return (
-		<nav className='flex min-w-1/10 items-center justify-between px-6 h-16 bg-yellow-100 shadow-md'>
-			<div>
-				{/* Logo or Brand Name */}
-				<a href='/' className='text-xl font-bold text-gray-800'>
-					Brand
-				</a>
-			</div>
-		</nav>
-	);
+  return (
+    <nav className='flex min-w-1/10 items-center justify-between px-6 h-16 bg-yellow-100 shadow-md'>
+      <div>
+        {/* Logo or Brand Name */}
+        <a href='/' className='text-xl font-bold text-gray-800'>
+          Brand
+        </a>
+      </div>
+    </nav>
+  );
 }
 
 export default NavBarWrapper;
