7a2ba4dd6fcef01f80cfcdd71289691e11935999
Joe Kim <joesm.genie@gmail.com>
2025-04-16 16:29:15 +0700

    set cookie in login api route

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,4 +1,6 @@
+import { ONE_HOUR } from '@/commons/constants';
 import { getAuthenticationService } from '@/lib/service';
+import { cookies } from 'next/headers';
 import { NextResponse, type NextRequest } from 'next/server';
 
 export async function POST(request: NextRequest, response: NextResponse) {
@@ -19,7 +21,17 @@ export async function POST(request: NextRequest, response: NextResponse) {
 			return NextResponse.json({ message: response.message }, { status: 401 });
 		}
 
-		return NextResponse.json(response, { status: 200 });
+		// Set cookies
+		(await cookies()).set({
+			name: 'token',
+			value: response.accessToken as string,
+			httpOnly: true,
+			secure: process.env.NODE_ENV === 'production',
+			sameSite: 'strict',
+			maxAge: ONE_HOUR,
+		});
+
+		return NextResponse.redirect(new URL('/', request.url), { status: 200 });
 	} catch (error) {
 		return NextResponse.json(
 			{ message: 'An error occurred during login' },
