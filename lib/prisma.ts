1fd940dc20402dfce6dd55b395e800d167a46e00
Joe Kim <joesm.genie@gmail.com>
2025-04-17 10:32:47 +0700

    fix database initializing after update dependencies

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,36 +6,24 @@ import {
 } from '@/commons/constants';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
 
-let pgPool: Pool | null = null;
 let prismaClient: PrismaClient | null = null;
-
-function getPool(): Pool {
-	if (!pgPool) {
-		console.log('Initializing new PostgreSQL connection pool');
-		pgPool = new Pool({
-			connectionString: DB_CONNECTION_STRING,
-			max: DB_POOL_MAX,
-			idleTimeoutMillis: DB_IDLE_TIMEOUT,
-			connectionTimeoutMillis: DB_CONNECTION_TIMEOUT,
-			allowExitOnIdle: true,
-		});
-
-		pgPool.on('error', (err) => {
-			console.error('Unexpected error on idle PostgreSQL client', err);
-			process.exit(-1); // Exit process with failure
-		});
-	}
-
-	return pgPool;
-}
 
 function getPrismaClient(): PrismaClient {
 	if (!prismaClient) {
 		console.log('Initializing Prisma client');
-		const pool = getPool();
-		const adapter = new PrismaPg(pool);
+		const adapter = new PrismaPg(
+			{
+				connectionString: DB_CONNECTION_STRING,
+				max: DB_POOL_MAX,
+				idleTimeoutMillis: DB_IDLE_TIMEOUT,
+				connectionTimeoutMillis: DB_CONNECTION_TIMEOUT,
+				allowExitOnIdle: true,
+			},
+			{
+				schema: 'public',
+			},
+		);
 		prismaClient = new PrismaClient({
 			adapter,
 			log: ['query', 'info', 'warn', 'error'],
@@ -52,13 +40,7 @@ async function closePrismaClient(): Promise<void> {
 		prismaClient = null;
 	}
 
-	if (pgPool) {
-		console.log('Closing PostgreSQL connection pool');
-		await pgPool.end();
-		pgPool = null;
-	}
-	
 	return Promise.resolve();
 }
 
-export { getPool, getPrismaClient, closePrismaClient };
+export { getPrismaClient, closePrismaClient };
