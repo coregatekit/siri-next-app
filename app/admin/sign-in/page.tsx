095e81a74dbd28d74e6010efef4cfa621296d2cc
Joe Kim <joesm.genie@gmail.com>
2025-04-17 14:48:56 +0700

    remove old sign in

--- app/admin/sign-in/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AdminLoginContainer from './container';
-
-function AdminLogin() {
-	return <AdminLoginContainer />;
-}
-
-export default AdminLogin;
--- app/sign-in/page.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import SignInContainer from './container';
-
-function SignIn() {
-	return <SignInContainer />;
-}
-
-export default SignIn;
