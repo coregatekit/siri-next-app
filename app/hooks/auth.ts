ded5b4fa501d4a54220a6f8fb813da70aa7120c3
Joe Kim <joesm.genie@gmail.com>
2025-04-17 09:45:37 +0700

    add header application json in login request

--- app/hooks/auth.ts
+++ app/hooks/auth.ts
@@ -16,6 +16,9 @@ export default function useAuth() {
 		try {
 			const response = await fetch(authRoute, {
 				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
 				body: JSON.stringify({ username, password }),
 			});
 
