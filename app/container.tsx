0c690f948b4f5c79016aab666f4e2f075c71874c
Joe Kim <joesm.genie@gmail.com>
2025-04-17 12:42:06 +0700

    update home page

---
+++ app/container.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function HomeContainer() {
+	return (
+		<div className='flex flex-col justify-center items-center gap-4 py-12'>
+			<h1 className='text-2xl font-bold text-slate-700'>
+				Welcome to Siri Resort
+			</h1>
+			<p className='text-slate-500'>
+				This is a sample application to demonstrate the usage of Next.js.
+			</p>
+		</div>
+	);
+}
