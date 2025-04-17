ef51b4c2e80ad6b41867c6aab882c9a02996dd9d
Joe Kim <joesm.genie@gmail.com>
2025-04-17 13:36:20 +0700

    update design to render menu list

--- app/container.tsx
+++ app/container.tsx
@@ -2,30 +2,56 @@ import Menu from '@/components/menu';
 import React from 'react';
 import type { MenuItem } from './types/menu';
 import Routes from '@/commons/routes';
+import PlusIcon from '@/components/icons/plus';
+import BedDoubleIcon from '@/components/icons/bed-double';
+import SlidersHorizontalIcon from '@/components/icons/sliders-horizontal';
+import HandCoinsIcon from '@/components/icons/hand-coins';
+import TodoListIcon from '@/components/icons/list-todo';
 
 const menuItems: MenuItem[] = [
 	{
 		name: 'Booking',
 		description: 'Book a room at Siri Resort',
 		href: Routes.BOOKING,
+		icon: <PlusIcon className='text-primary' />,
 	},
 	{
 		name: 'Rooms',
 		description: 'View available rooms',
 		href: Routes.ROOMS,
+		icon: <BedDoubleIcon className='text-primary' />,
 	},
 	{
 		name: 'Management',
 		description: 'Manage the resort operations',
 		href: Routes.MANAGEMENT,
+		icon: <SlidersHorizontalIcon className='text-primary' />,
 	},
 	{
 		name: 'Payments',
 		description: 'Summarize the payments',
 		href: Routes.PAYMENTS,
+		icon: <HandCoinsIcon className='text-primary' />,
 	},
 	{
 		name: 'Todo',
+		description: 'Manage your tasks',
+		href: Routes.TODO,
+		icon: <TodoListIcon className='text-primary' />,
+	},
+
+	{
+		name: 'Managementx',
+		description: 'Manage the resort operations',
+		href: Routes.MANAGEMENT,
+	},
+	{
+		name: 'Paymentsx',
+		description: 'Summarize the payments',
+		href: Routes.PAYMENTS,
+	},
+	{
+		name: 'Todox',
 		description: 'Manage your tasks',
 		href: Routes.TODO,
 	},
@@ -41,7 +67,7 @@ export default function HomeContainer() {
 				This is a sample application to demonstrate the usage of Next.js.
 			</p>
 
-			<Menu />
+			<Menu menus={menuItems} />
 		</div>
 	);
 }
--- components/menu.tsx
+++ components/menu.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import type { MenuItem } from '@/app/types/menu';
+import { Card } from './ui/card';
+import Link from 'next/link';
 
-export default function Menu() {
-	return <div>Menu</div>;
+type Props = {
+	menus: MenuItem[];
+};
+
+export default function Menu({ menus }: Props) {
+	return (
+		<div className='flex flex-wrap w-4/6 justify-center'>
+			{menus.map((menu) => (
+				<Card
+					key={menu.name}
+					className=' w-52 h-48 m-4 p-4 bg-white shadow-md hover:shadow-xl transition-shadow duration-300'
+				>
+					<Link
+						href={menu.href}
+						className='flex flex-col gap-2 items-start justify-center h-full'
+					>
+						<span>{menu.icon}</span>
+						<h2 className='text-lg font-semibold text-slate-700'>
+							{menu.name}
+						</h2>
+						<p className='text-sm text-slate-500'>{menu.description}</p>
+					</Link>
+				</Card>
+			))}
+		</div>
+	);
 }
