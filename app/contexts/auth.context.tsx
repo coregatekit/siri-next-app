1f47f24e4d415b7b0bebd9aacaa7875f230ecdf5
Joe Kim <joesm.genie@gmail.com>
2025-04-17 22:37:47 +0700

    create auth context

---
+++ app/contexts/auth.context.tsx
@@ -0,0 +1,102 @@
+'use client';
+
+import {
+	createContext,
+	useContext,
+	useEffect,
+	useState,
+	type ReactNode,
+} from 'react';
+import type { UserData } from '../types/auth';
+import { useRouter } from 'next/router';
+
+type AuthContextType = {
+	isAuthenticated: boolean;
+	isLoading: boolean;
+	error: string | null;
+	login: (username: string, password: string) => Promise<void>;
+	logout: () => Promise<void>;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export function AuthProvider({ children }: { children: ReactNode }) {
+	const [user, setUser] = useState<UserData | null>(null);
+	const [error, setError] = useState<string | null>(null);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
+	const router = useRouter();
+
+	const login = async (username: string, password: string) => {
+		setIsLoading(true);
+		try {
+			const response = await fetch('/api/auth/login', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ username, password }),
+			});
+
+			if (!response.ok) {
+				const errorData = await response.json();
+				setError(errorData.message);
+				console.log(errorData);
+			}
+
+			const userResponse = await fetch('/api/auth/me');
+			if (userResponse.ok) {
+				const userData = await userResponse.json();
+				setUser(userData);
+				router.push('/');
+			}
+		} catch (error: unknown) {
+			setError(
+				error instanceof Error ? error.message : 'An unknown error occurred',
+			);
+			console.error('Login error:', error);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
+	const logout = async () => {
+		try {
+			const response = await fetch('/api/auth/logout', {
+				method: 'POST',
+			});
+
+			if (!response.ok) {
+				const errorData = await response.json();
+				console.log(errorData);
+				setError(errorData.error || 'An unknown error occurred');
+				return;
+			}
+
+			setUser(null);
+			router.push('/login');
+		} catch (error: unknown) {
+			setError(
+				error instanceof Error ? error.message : 'An unknown error occurred',
+			);
+			console.error('Logout error:', error);
+		}
+	};
+
+	return (
+		<AuthContext.Provider
+			value={{ isAuthenticated: !!user, isLoading, error, login, logout }}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
+}
+
+export default function useAuth() {
+	const context = useContext(AuthContext);
+
+	if (!context) {
+		throw new Error('useAuth must be used within an AuthProvider');
+	}
+
+	return context;
+}
