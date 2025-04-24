8f1c5a767446de7fe8ad332b96b34f77255589dd
Joe Kim <joesm.genie@gmail.com>
2025-04-24 13:31:36 +0700

    feat: implement accordion for room types management

---
+++ app/managements/components/system-management-accordion.tsx
@@ -0,0 +1,19 @@
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '@/components/ui/accordion';
+import React from 'react';
+
+export default function SystemManagementAccordion() {
+	return (
+		<Accordion type='single' collapsible className='rounded-md w-full bg-accent p-6'>
+			<AccordionItem value='item-1'>
+				<AccordionTrigger className='text-primary text-lg'>Room Types</AccordionTrigger>
+				<AccordionContent>
+				</AccordionContent>
+			</AccordionItem>
+		</Accordion>
+	);
+}
--- app/managements/settings/container.tsx
+++ app/managements/settings/container.tsx
@@ -1,21 +1,11 @@
-import type { MenuItem } from '@/app/types/menu';
-import Menu from '@/components/menu';
 import React from 'react';
+import SystemManagementAccordion from '../components/system-management-accordion';
 
-const menuItems: MenuItem[] = [
-	{
-		name: 'Room Types',
-		description: 'Manage the room types',
-		href: '',
-	},
-];
 export default function SettingsContainer() {
 	return (
-		<div className='flex flex-col mt-10 mx-12'>
+		<div className='flex flex-col mt-10 mx-12 w-full'>
 			<div className='text-2xl font-bold mb-12'>System Settings</div>
-			<div className='w-full'>
-				<Menu menus={menuItems} className='w-full'/>
-			</div>
+			<SystemManagementAccordion />
 		</div>
 	);
 }
