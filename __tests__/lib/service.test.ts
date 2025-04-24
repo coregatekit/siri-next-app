2208474d043bf5e0325c7e40c5b8b6e6f8e8cbc8
Joe Kim <joesm.genie@gmail.com>
2025-04-24 15:07:35 +0700

    test: add unit test for get service instance of authentication todo and room type

--- __tests__/lib/service.test.ts
+++ __tests__/lib/service.test.ts
@@ -2,9 +2,15 @@ import {
 	getEncryptionService,
 	closeServices,
 	getEmployeeService,
+	getAuthenticationService,
+	getTodoService,
+	getRoomTypeService,
 } from '@/lib/service';
 import { EncryptionService } from '@/services/encryptions';
 import { EmployeeService } from '@/services/employees';
+import { AuthenticationService } from '@/services/authentications';
+import { TodoService } from '@/services/todos';
+import { RoomTypeService } from '@/services/room-types';
 
 // Mock PrismaClient to avoid browser environment issues
 jest.mock('@prisma/client', () => {
@@ -46,6 +52,33 @@ describe('Service', () => {
 
 			expect(service).toBeDefined();
 			expect(service).toBeInstanceOf(EmployeeService);
+		});
+	});
+
+	describe('getAuthenticationService', () => {
+		it('should return an instance of AuthenticationService', () => {
+			const service = getAuthenticationService();
+
+			expect(service).toBeDefined();
+			expect(service).toBeInstanceOf(AuthenticationService);
+		});
+	});
+
+	describe('getTodoService', () => {
+		it('should return an instance of TodoService', () => {
+			const service = getTodoService();
+
+			expect(service).toBeDefined();
+			expect(service).toBeInstanceOf(TodoService);
+		});
+	});
+
+	describe('getRoomTypeService', () => {
+		it('should return an instance of RoomTypeService', () => {
+			const service = getRoomTypeService();
+
+			expect(service).toBeDefined();
+			expect(service).toBeInstanceOf(RoomTypeService);
 		});
 	});
 });
