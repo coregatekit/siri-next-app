87bed20329b4af8c80e0c6b2c304bcbf8bcd4c4d
Joe Kim <joesm.genie@gmail.com>
2025-04-24 11:32:38 +0700

    style: update page profile setting

--- app/profile/settings/container.tsx
+++ app/profile/settings/container.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
 export default function ProfileSettingsContainer() {
-	return <div>ProfileSettingsContainer</div>;
+	return (
+		<div className='flex flex-col items-center justify-center h-screen'>
+			<div className='text-2xl font-bold'>Profile Settings</div>
+			<div>Under constructions.</div>
+		</div>
+	);
 }
