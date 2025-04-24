a5636d10e90d560b4a786fe04d3e8f4ca6fae6f3
Joe Kim <joesm.genie@gmail.com>
2025-04-24 13:38:46 +0700

    feat: add data table for room type

--- app/managements/components/manage-room-types.tsx
+++ app/managements/components/manage-room-types.tsx
@@ -1,3 +1,5 @@
+import { Button } from '@/components/ui/button';
+import { DataTable } from '@/components/ui/data-table';
 import type { ColumnDef } from '@tanstack/react-table';
 import React from 'react';
 
@@ -6,24 +8,38 @@ const RoomTypesColumn: ColumnDef<RoomType>[] = [
 		accessorKey: 'id',
 		header: 'ID',
 	},
-  {
-    accessorKey: 'name',
-    header: 'Name',
-  },
-  {
-    accessorKey: 'detail',
-    header: 'Detail',
-  },
-  {
-    accessorKey: 'createdAt',
-    header: 'Created At',
-  },
-  {
-    accessorKey: 'updatedAt',
-    header: 'Updated At',
-  },
+	{
+		accessorKey: 'name',
+		header: 'Name',
+	},
+	{
+		accessorKey: 'detail',
+		header: 'Detail',
+	},
+	{
+		accessorKey: 'createdAt',
+		header: 'Created At',
+	},
+	{
+		accessorKey: 'updatedAt',
+		header: 'Updated At',
+	},
+	{
+		accessorKey: 'action',
+		header: 'Action',
+		// cell: ({ row }) => (
+		// 	<div className='flex gap-2'>
+		// 		<Button className='text-blue-500'>Edit</Button>
+		// 		<Button className='text-red-500'>Delete</Button>
+		// 	</div>
+		// ),
+	},
 ];
 
 export default function ManageRoomTypes() {
-	return <div>ManageRoomTypes</div>;
+	return (
+		<div className='rounded-md bg-white shadow-md'>
+			<DataTable columns={RoomTypesColumn} data={[]} />
+		</div>
+	);
 }
