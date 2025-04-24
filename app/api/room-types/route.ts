9d74b40a683175a148c67beeb81b393a9db5c3e9
Joe Kim <joesm.genie@gmail.com>
2025-04-24 15:26:09 +0700

    feat: add api route create room type

--- app/api/room-types/route.ts
+++ app/api/room-types/route.ts
@@ -17,3 +17,25 @@ export async function GET(request: NextRequest) {
 		);
 	}
 }
+
+export async function POST(request: NextRequest) {
+	try {
+		const { name, detail } = await request.json();
+
+		const roomTypeService = getRoomTypeService();
+		const roomType = await roomTypeService.createRoomType({
+			name,
+			detail,
+		});
+
+		return NextResponse.json(roomType, { status: 201 });
+	} catch (error) {
+		if (error instanceof Error) {
+			return NextResponse.json({ message: error.message }, { status: 500 });
+		}
+		return NextResponse.json(
+			{ message: 'Internal server error' },
+			{ status: 500 },
+		);
+	}
+}
